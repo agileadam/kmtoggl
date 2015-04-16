c7114056fd7a0445b904ba8d5543008180afc301
Adam Courtemanche <adam.courtemanche@gmail.com>
2015-04-16 11:14:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Requirements:
+
+1. Requests - http://docs.python-requests.org/en/latest/
+1. Keyboard Maestro
+1. KMLink URL Handler (link/instructions below)
+1. A working Toggl account with a working API token
+
+# Intro:
+
+AciveCollab is a time management system. ActiveCollab has projects, and projects have tasks within them.
+
+Toggl is a web-based time tracking application.
+
+Keyboard Maestro is an automation application for the Mac.
+
+Using this python script, a bookmarklet, and Keyboard Maestro, you can one-click start a Toggl timer for a task
+or project in ActiveCollab. Some advantages to the bookmarklet are:
+
+1. Automatically creates the ActiveCollab project in Toggl (saving several steps) if it doesn't already exist
+1. Time entries it creates are compatible with https://github.com/agileadam/actoggltimeimport
+1. Makes it almost effortless to track your time on ActiveCollab projects and tasks
+
+# Setup
+
+## Toggl configuration
+
+1. Create `~/.togglrc` file: (use your own token)<pre>[toggl]<br/>token=e0926359d8c73bbe7ab136d042530d9a</pre>
+
+## KMLink URL Handler
+
+You will need to have KMLink working. Please follow just the "Installation" instructions here: http://agileadam.com/2015/03/kmlink/
+
+## Keyboard Maestro Macro
+
+This repository contains a macro that you should be able to import into Keyboard Maestro.
+
+1. Import the macro into Keyboard Maestro
+1. Update the "Execute Shell Script" to point to your copy of kmtoggl.py
+
+## Bookmarklet
+
+1. Copy this string into a text editor:
+    1. `javascript:(function()%7Bfunction callback()%7B(function(%24)%7Bvar jQuery%3D%24%3Bvar pathArray %3D window.location.pathname.split( '%2F' )%3Bproject %3D ""%3Bif (pathArray%5B2%5D) %7Bproject %3D pathArray%5B2%5D%3B%7Dvar time_description %3D "General"%3Bvar task_num %3D ""%3Bif (pathArray%5B3%5D %3D%3D 'tasks' %26%26 pathArray%5B4%5D) %7Bvar time_description %3D %24('%23breadcrumbs li%3Anth-child(4) a').text()%3Btask_num %3D pathArray%5B4%5D%3B%7Dvar final_url %3D "kmlink%3A%2F%2Fcom.apple.Applescript.KMLink%3Fmacro%3D676619D3-CE3A-4DE0-A721-C8FD5025C50D%26project%3D" %2B encodeURIComponent(project) %2B "%26time_description%3D" %2B encodeURIComponent(time_description) %2B "%26task_num%3D" %2B task_num%3Bwindow.location %3D final_url%7D)(jQuery.noConflict(true))%7Dvar s%3Ddocument.createElement("script")%3Bs.src%3D"https%3A%2F%2Fajax.googleapis.com%2Fajax%2Flibs%2Fjquery%2F1.7.1%2Fjquery.min.js"%3Bif(s.addEventListener)%7Bs.addEventListener("load"%2Ccallback%2Cfalse)%7Delse if(s.readyState)%7Bs.onreadystatechange%3Dcallback%7Ddocument.body.appendChild(s)%3B%7D)()`
+1. Change "676619D3-CE3A-4DE0-A721-C8FD5025C50D" to the UID of your imported Keyboard Maestro macro.
+    1. You get the UID of the macro by highlighting it in Keyboard Maestro, then using the menu item "Edit > Copy As > Copy UID"
+1. Create a new bookmark (probably should go in your bookmarks toolbar) with the javascript text you edited above.
+
+# Usage
+
+1. Click the bookmark.
+1. Check if the timer started (either in Toggl Desktop or on the toggl website)
---
+++ kmtoggl.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import json
+import argparse
+import requests
+import os
+
+config = ConfigParser.RawConfigParser()
+try:
+    config.read(os.path.expanduser('~/.togglrc'))
+    toggl_api_token = config.get('toggl', 'token')
+except:
+    LOG.error('Please create a ~/.togglrc file with your token. View README for setup instructions.')
+
+
+def toggl_query(url, params, method, payload=None):
+    api_url = 'https://www.toggl.com/api/v8' + url
+    auth = (toggl_api_token, 'api_token')
+    headers = {'content-type': 'application/json'}
+
+    if method == "POST":
+        response = requests.post(api_url, auth=auth, headers=headers, params=params, data=payload)
+    elif method == "PUT":
+        response = requests.put(api_url, auth=auth, headers=headers, params=params)
+    elif method == "GET":
+        response = requests.get(api_url, auth=auth, headers=headers, params=params)
+    else:
+        raise UserWarning('GET, POST and PUT are the only supported request methods.')
+
+    # If the response errored, raise for that.
+    if response.status_code != requests.codes.ok:
+        response.raise_for_status()
+
+    return response.json()
+
+
+def get_workspaces():
+    return toggl_query('/workspaces', None, 'GET')
+
+
+def get_workspace_projects(workspace_id):
+    return toggl_query('/workspaces/' + workspace_id + '/projects', None, 'GET')
+
+
+def create_project(project_name, workspace_id):
+    project_data = {"name": project_name, 'wid': workspace_id}
+    project_data_json = json.dumps({'project': project_data})
+    return toggl_query('/projects', None, 'POST', project_data_json)
+
+
+def get_running_timer():
+    return toggl_query('/time_entries/current', None, 'GET')
+
+
+def stop_timer(timer_id):
+    return toggl_query('/time_entries/' + str(timer_id) + '/stop', None, 'PUT')
+
+
+def start_timer(description, project_id):
+    time_entry_data = {"description": description, 'pid': project_id, 'created_with': 'kmtoggl'}
+    time_entry_data_json = json.dumps({'time_entry': time_entry_data})
+    return toggl_query('/time_entries/start', None, 'POST', time_entry_data_json)
+
+
+workspaces = get_workspaces()
+workspace_id = str(workspaces[0]['id'])
+
+# Build a dict of existing projects
+workspace_projects = get_workspace_projects(workspace_id)
+all_projects = {}
+for project in workspace_projects:
+    all_projects[project['name']] = project['id']
+
+# Get the values from the command line arguments
+project_name = str(os.environ['KMVAR_Project'])
+task_name = str(os.environ['KMVAR_Time_entry_description'])
+
+if not project_name or not task_name:
+    raise UserWarning('Could not get project or time entry description from KMToggl')
+
+project_id = None
+# Get a project ID for the time entry
+if project_name in all_projects:
+    project_id = all_projects[project_name]
+else:
+    # Create the project
+    project = create_project(project_name, workspace_id)
+    project_id = project['data']['id']
+
+if project_id is not None:
+    # Get running time entry
+    running_timer = get_running_timer()
+    if running_timer['data'] is not None:
+        stop_timer(running_timer['data']['id'])
+
+    # Start a new time entry
+    start_timer(task_name, project_id)
+    print "Started timer for", task_name, "in project", project_name
+
+exit()
